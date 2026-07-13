eede83043980f7d554f00a777c7d48ecabcc8219
Vierdant <vierdant@gmail.com>
2026-07-13 11:58:48 +0200

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,25 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from "@tailwindcss/vite";
 import favicons from "astro-favicons";
+import node from "@astrojs/node";
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   site: "https://yamikhal.com",
   integrations: [mdx(),favicons()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "de"],
-		routing: {
-			prefixDefaultLocale: true,
-		},
+
+  i18n: {
+      defaultLocale: "en",
+      locales: ["en", "de"],
+      routing: {
+          prefixDefaultLocale: true,
+      },
 	},
 });
